230db1cf773268dbd5f02c0bcd367efca7e6aa53
NOBAN HASAN <nobanhasan@gmail.com>
2022-04-17 04:47:49 +0600

    file added

---
+++ fieldValidator/build/generated/source/buildConfig/debug/com/noban/fieldvalidator/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.noban.fieldvalidator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.noban.fieldvalidator";
+  public static final String BUILD_TYPE = "debug";
+}
